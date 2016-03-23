365c92ca5a5d48874d638fe7606ada50f619bc38
pcrtncml <pcrtncml@gmail.com>
2016-03-23 09:45:00 -0500

    ejem

--- appdos/urls.py
+++ appdos/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from contactos.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
