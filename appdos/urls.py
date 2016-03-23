c3c5345f0c5d4d7e0f0343dd4681b686838b562a
pcrtncml <pcrtncml@gmail.com>
2016-03-23 09:12:34 -0500

    ejem

--- appdos/urls.py
+++ appdos/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^contactos/', include('contactos.urls')),
+    url(r'^$', contactos.views.co_ini, name='co_ini'),
 ]
