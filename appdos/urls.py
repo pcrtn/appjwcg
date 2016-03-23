5170f76886134ecb28c89263872a9f1709cebbf7
pcrtncml <pcrtncml@gmail.com>
2016-03-23 09:05:11 -0500

    init

--- appdos/urls.py
+++ appdos/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('contactos.urls')),
+    url(r'^contactos', include('contactos.urls')),
 ]
